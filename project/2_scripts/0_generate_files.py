e098b72157128c24622e85d23e978399f438b397
Ezequiel Paulo <ezequielcabeja@gmail.com>
2026-03-25 04:17:26 +0000

    additional comments

--- project/2_scripts/0_generate_files.py
+++ project/2_scripts/0_generate_files.py
@@ -1,21 +1,22 @@
-import os
+import os # Módulo para interagir com o sistema operacional, como criar diretórios e arquivos
 
 # Tamanhos em bytes
 file_sizes = [8, 64, 512, 4096, 32768, 262144, 2097152]
 
-output_dir = "../1_data"
+output_dir = "../1_data" # Diretório onde os ficheiros serão salvos
 
-os.makedirs(output_dir, exist_ok=True)
-counter = 0
+os.makedirs(output_dir, exist_ok=True) # Cria o diretório se ele não existir
+counter = 0 #contador para mostrar o número do ficheiro criado, não tem impacto na geração dos ficheiros
 
 for size in file_sizes:
     filename = f"{output_dir}/file_{size}B.bin"
-    counter += 1
+    counter += 1 
+
     # Dados aleatórios seguros
     data = os.urandom(size)
     
-    with open(filename, "wb") as f:
-        f.write(data)
+    with open(filename, "wb") as f: # Abre o arquivo para escrita em modo binário
+        f.write(data) # Escreve os dados aleatórios no arquivo
     
     print(f"{counter}º Ficheiro criado: {filename} ({size} bytes)")
 
